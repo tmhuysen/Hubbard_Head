0aadc99c9599b487cd18cb1a7a453e6b33d0117c
Tm Huysen <Tobias.Huysentruyt@Ugent.be>
2018-02-19 21:25:31 +0100

    made usable
    Former-commit-id: 4dc6757f1da6dab7fef4164d48a68e95c03696c6

---
+++ include/hubbardhead.h
@@ -0,0 +1,12 @@
+#ifndef HUBBARD_HEAD_HUBBARDHEAD_HPP
+#define HUBBARD_HEAD_HUBBARDHEAD_HPP
+
+
+
+#include "version.hpp"
+#include "io.h"
+#include "Hubbard.h"
+#include "OneRDM.h"
+
+#endif //HUBBARD_HEAD_HUBBARDHEAD_HPP
+
